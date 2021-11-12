c042fd6cd3b9eb9d4e8cf522ad795bd85e3f83bd
Greck-18 <greck1111@mail.ru>
2021-11-12 16:44:12 +0300

    [Fix] Security file,__manifest__

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,27 @@
 {
-    'name': "Task Tracker",
-    'description': 'Chto-to',
-    'author': 'depressed tilted',
-    'depends': ['hr'],
+    'name': 'Task Tracker',
+    'version': '1.1',
+    'summary': '',
+    'sequence': 1,
+    'description': 'Task tracker',
+    'category': '',
+    'website': '',
+    'images': [
+    ],
+    'depends': ['base', 'hr'],
     'data': [
         'data/default_stages.xml',
-        'view/hr_employee_view.xml',
         'view/task_view.xml',
+        'view/hr_employee_view.xml',
         'view/project_view.xml',
         'security/ir.model.access.csv',
     ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
     'installable': True,
-    'application': True
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
