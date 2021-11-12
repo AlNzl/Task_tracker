ca7b90de1ffde34d7f6e82cfc3163260cbc7375b
sadenisqua <an1starean@icloud.com>
2021-11-12 14:58:31 +0300

    denis ebet

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Task Tracker",
+    'description': 'Chto-to',
+    'author': 'depressed tilted',
+    'data': [
+    ],
+    'installable': True,
+    'application': True
+}
---
+++ model/connected_models/__init__.py
@@ -0,0 +1,4 @@
+from . import project
+from . import stage
+from . import task
+from . import time_tracker
