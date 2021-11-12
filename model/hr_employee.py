fc433e43f2401b72a0cf89109349d1e4231fab62
sadenisqua <an1starean@icloud.com>
2021-11-12 15:14:55 +0300

    [Fix] position(field) fixed

--- model/hr_employee.py
+++ model/hr_employee.py
@@ -7,6 +7,6 @@ class HrEmployee(models.Model):
     employee_coefficient = fields.Float(string="Employee coefficient")
     currency_id = fields.Many2one("res.currency", string="currency")
     employee_hour = fields.Monetary(string="Employee hour")
-    position = fields.Selection(selection=[('Team Lead', 'Team Lead'), ('PM', 'PM'), ('Developer', 'Developer'), ('BA', 'BA')])
+    position = fields.Selection(selection=[('team_lead', 'Team Lead'), ('pm', 'PM'), ('developer', 'Developer'), ('ba', 'BA')])
 
     worker_ids = fields.One2many(comodel_name="task", inverse_name="worker_id", string="Workers")
