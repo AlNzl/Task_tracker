124494ea18e75c8a5bbf596fae13780b10f6ad8a
Greck-18 <greck1111@mail.ru>
2021-11-13 13:19:25 +0300

    Add view for project and change model project

--- model/connected_models/project.py
+++ model/connected_models/project.py
@@ -1,26 +1,33 @@
 from odoo import models, fields, api
 
+AVAILABLE_PRIORITIES = [
+    ('0', 'Low'),
+    ('1', 'Important'),
+]
+
 
 class Project(models.Model):
     _name = "project"
     _description = "Project"
 
-    name = fields.Char(string="Project name")
+    name = fields.Char(string="Project name", required="True")
     description = fields.Text(string="Description")
+    date_of_registration = fields.Datetime(string="Date", default=lambda self: fields.datetime.now())
     currency_id = fields.Many2one(comodel_name="res.currency", string="currency")
     total_price = fields.Monetary(string="Total Price")
     time = fields.Float(string="General time")
+    priority = fields.Selection(AVAILABLE_PRIORITIES, select=True)
     worker_ids = fields.Many2many(comodel_name="hr.employee", string="Workers")
-    team_lead = fields.Many2one(comodel_name="hr.employee", string="Team Lead")
-    project_manager = fields.Many2one(comodel_name="hr.employee", string="Project manager")
-    #price_for_hour = fields.Float(string="Price for hour")  # TODO make compute field
+    team_lead_id = fields.Many2one(comodel_name="hr.employee", string="Team Lead")
+    project_manager_id = fields.Many2one(comodel_name="hr.employee", string="Project Manager")
     task_ids = fields.One2many(comodel_name="task", inverse_name="project_id", string="Tasks")
     project_line_ids = fields.One2many(comodel_name="project.line", inverse_name="project_id", string="Workers")
+
 
 class ProjectLine(models.Model):
     _name = "project.line"
     _description = "Project Line"
 
-    employee_id = fields.Many2one(comodel_name="hr.employee",string="Employee")
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee")
     sold = fields.Float(string="Sold")
-    project_id = fields.Many2one(comodel_name="project",string="Worker")
+    project_id = fields.Many2one(comodel_name="project", string="Worker")
