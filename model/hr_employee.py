b4ddca41bf6dec0f2d3c06eec60ab244a36b82fd
kopelevnikita <40999657+kopelevnikita@users.noreply.github.com>
2021-11-15 09:30:39 +0300

    Dev (#19)
    * Delete model 'Department' and create 'reference_book', add view
    
    * Delete model 'Department' and create 'reference_book', add view
    
    * Delete model 'Department' and create 'reference_book', add view v.2
    
    * auto select TL and PM

--- model/hr_employee.py
+++ model/hr_employee.py
@@ -7,6 +7,10 @@ class HrEmployee(models.Model):
     employee_coefficient = fields.Float(string="Employee coefficient")
     currency_id = fields.Many2one("res.currency", string="currency")
     employee_hour = fields.Monetary(string="Employee hour")
-    position = fields.Selection(selection=[('team_lead', 'Team Lead'), ('pm', 'PM'), ('developer', 'Developer'), ('ba', 'BA')])
+    # position = fields.Selection(selection=[('team_lead', 'Team Lead'), ('pm', 'PM'), ('developer', 'Developer'), ('ba', 'BA'), ('dev_tl', 'Developer or Team Lead')])
 
-    worker_ids = fields.One2many(comodel_name="task", inverse_name="worker_id", string="Workers")
+    task_ids = fields.One2many(comodel_name="task", inverse_name="worker_id", string="Tasks")  # Tut taski dolzni bit'
+
+    position_ids = fields.Many2many(comodel_name="reference.book", string="Positions")
+
+
