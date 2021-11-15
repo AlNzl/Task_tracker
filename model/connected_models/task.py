8af7ec040bc95e17032df727215d1a1cf8e6d05b
LubaMazurkevich <lubou.mazurkevich@gmail.com>
2021-11-15 17:27:00 +0300

    [FIX] function _change_stage ,added function create_stage_dct

--- model/connected_models/task.py
+++ model/connected_models/task.py
@@ -1,10 +1,6 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-
-AVAILABLE_PRIORITIES = [
-    ('low', 'Low'),
-    ('important', 'Important'),
-]
+from .project import AVAILABLE_PRIORITIES
 
 
 class Task(models.Model):
@@ -13,6 +9,9 @@ class Task(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
 
     def _get_default_stage_id(self):
+        """
+        Get default stage
+        """
         stage_id = self.env.ref("Task_tracker.task_stage_backlog").id
         return stage_id
 
@@ -26,18 +25,46 @@ class Task(models.Model):
     worker_id = fields.Many2one(comodel_name="hr.employee", string="Worker")
     responsible_id = fields.Many2one(comodel_name="hr.employee", string="Responsible person")
     project_id = fields.Many2one(comodel_name="project", string="Project", ondelete="cascade")
-    time_tracker_line_ids = fields.One2many(comodel_name="time.tracker.line", inverse_name="task_id", string="Time tracker")
+    time_tracker_line_ids = fields.One2many(comodel_name="time.tracker.line", inverse_name="task_id",
+                                            string="Time tracker")
+
+    def create_stage_dct(self):
+        """
+        Create dict with stages
+        """
+        back = self.env.ref("Task_tracker.task_stage_backlog").id
+        ready = self.env.ref("Task_tracker.task_stage_ready").id
+        progress = self.env.ref("Task_tracker.task_stage_progress").id
+        review = self.env.ref("Task_tracker.task_stage_review").id
+        test = self.env.ref("Task_tracker.task_stage_test").id
+        done = self.env.ref("Task_tracker.task_stage_done").id
+
+        stage_dct = {
+            back: {"next": ready},
+            ready: {"previous": back, "next": progress},
+            progress: {"previous": ready, "next": review},
+            review: {"previous": test, "next": progress},
+            test: {"previous": review, "next": done}
+        }
+        return stage_dct
 
     @api.model
     def _change_stage(self):
-        if len(self.stage_id) == 1 and self.stage_id.id < self.env.ref("Task_tracker.task_stage_done").id:
-            stage_id = self.stage_id.id + 1
+        """
+        Change stage on tree view if currents stages the same
+        """
+        stage_dct = self.create_stage_dct()
+        if all((len(self.stage_id) == 1, self.stage_id.id in stage_dct, self.stage_id.id != self.env.ref("Task_tracker.task_stage_done").id)):
+            stage_id = stage_dct[self.stage_id.id]["next"]
             self.env["task"].browse(self._context.get("active_ids")).update({"stage_id": stage_id})
         else:
-            raise UserError("The stage can't be changed.")
+            raise UserError(_("The stage can't be changed."))
 
     @api.model
     def _read_group_stage_ids(self, stages, domain, order):
+        """
+        Group stage_ids default
+        """
         stage_ids = self.env['stage'].search([])
         return stage_ids
 
