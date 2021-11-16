87984b161a27511ecda365d2eaa0856816b271f1
sadenisqua <88110189+sadenisqua@users.noreply.github.com>
2021-11-16 14:40:03 +0300

    [Add] made a task to limit the creation of a model Time tracker (#31)
    * [Add] made a task to limit the creation of a model Time tracker
    
    * [fix] fix func write
    
    * [fix] update documentation
    
    * [fix] add translate

--- model/connected_models/task.py
+++ model/connected_models/task.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from .project import AVAILABLE_PRIORITIES
+from datetime import datetime, timedelta
 
 
 class Task(models.Model):
@@ -21,15 +22,20 @@ class Task(models.Model):
     stage_id = fields.Many2one(comodel_name="stage", string="Stage", default=_get_default_stage_id,
                                track_visibility="onchange", group_expand="_read_group_stage_ids")
     worker_id = fields.Many2one(comodel_name="hr.employee", string="Worker",
-                                domain=lambda self: [("position_ids.id", "=", self.env.ref("Task_tracker.reference_book_developer").id)])
+                                domain=lambda self: [
+                                    ("position_ids.id", "=", self.env.ref("Task_tracker.reference_book_developer").id)])
     responsible_id = fields.Many2one(comodel_name="hr.employee", string="Responsible person",
-                                     domain=lambda self: [("position_ids.id", "=", self.env.ref("Task_tracker.reference_book_team_lead").id)])
+                                     domain=lambda self: [("position_ids.id", "=",
+                                                           self.env.ref("Task_tracker.reference_book_team_lead").id)])
     project_id = fields.Many2one(comodel_name="project", string="Project", ondelete="cascade")
     time_tracker_line_ids = fields.One2many(comodel_name="time.tracker.line", inverse_name="task_id",
                                             string="Time tracker")
+    timer = fields.Datetime(string="Timer")
 
     def create_stage_dct(self):
-        """Create dict with stages"""
+        """
+        Create dict with stages
+        """
         back = self.env.ref("Task_tracker.task_stage_backlog").id
         ready = self.env.ref("Task_tracker.task_stage_ready").id
         progress = self.env.ref("Task_tracker.task_stage_progress").id
@@ -48,7 +54,9 @@ class Task(models.Model):
 
     @api.model
     def change_stage(self):
-        """Change stage on tree view if currents stages the same"""
+        """
+        Change stage on tree view if currents stages the same
+        """
         stage_dct = self.create_stage_dct()
         acceptance_criteria = [len(self.stage_id) == 1,
                                self.stage_id.id in stage_dct,
@@ -62,7 +70,9 @@ class Task(models.Model):
 
     @api.model
     def _read_group_stage_ids(self, stages, domain, order):
-        """Group stage_ids default"""
+        """
+        Group stage_ids default
+        """
         stage_ids = self.env["stage"].search([])
         return stage_ids
 
@@ -80,6 +90,25 @@ class Task(models.Model):
         """
         for record in self:
             record.total_time = record.ba_time * record.worker_id.employee_coefficient
+
+    @api.constrains("stage_id")
+    def check_stage(self):
+        """
+        Here we check the stage, if it is in 'In progress', we start the timer
+        """
+        if self.stage_id.id == self.env.ref("Task_tracker.task_stage_progress").id:
+            timer = datetime.now() + timedelta(hours=self.total_time, days=1)
+            self.timer = timer
+
+    def write(self, vals):
+        """
+        If datetime now < timer, we are not allowed to change
+        """
+        if self.timer < datetime.now():
+            raise UserError(_("You can no longer change Time tracker"))
+        if not self.timer or self.timer > datetime.now():
+            res = super(Task, self).write(vals)
+            return res
 
 
 class TimeTrackerLine(models.Model):
